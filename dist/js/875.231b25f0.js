0fcc7b3ef6665c5f1d91e26615b24599eda55ad3
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2024-02-05 21:48:19 +0100

    commit prototip na main

---
+++ dist/js/875.231b25f0.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvoter_frontend"]=self["webpackChunkvoter_frontend"]||[]).push([[875],{2875:function(n,t,e){e.r(t),e.d(t,{default:function(){return f}});var u=function(){var n=this;n._self._c;return n._m(0)},r=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],s=e(1001),a={},l=(0,s.Z)(a,u,r,!1,null,null,null),f=l.exports}}]);
+//# sourceMappingURL=875.231b25f0.js.map
